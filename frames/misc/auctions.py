abbe723c61243144e164e5c5b3e3a948123f5437
Jason Hunter <hunterjm@gmail.com>
2015-11-24 23:35:22 -0500

    Add scrollbar to watchlist frame

--- frames/misc/auctions.py
+++ frames/misc/auctions.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import tkinter.ttk as ttk
 from enum import Enum
 
@@ -15,28 +16,37 @@ class Auctions():
     cards = {}
 
     def __init__(self, frame):
-        t = ttk.Treeview(frame, columns=('timestamp', 'initial', 'current', 'bin', 'expires'), selectmode='browse')
-        t.column("#0", width=75)
-        t.column("timestamp", width=100)
-        t.column("initial", width=50)
-        t.column("current", width=50)
-        t.column("bin", width=50)
-        t.column("expires", width=50)
+        self.view = tk.Frame(frame)
+        self.view.grid_rowconfigure(0, weight=1)
+        self.view.grid_columnconfigure(0, weight=1)
+        self.view.grid_columnconfigure(1, weight=0)
 
-        t.heading("#0", text="Name", anchor="w")
-        t.heading("timestamp", text="Time")
-        t.heading("initial", text="Initial Bid")
-        t.heading("current", text="Current Bid")
-        t.heading("bin", text="BIN")
-        t.heading("expires", text="Expires")
+        self.tree = ttk.Treeview(self.view, columns=('timestamp', 'initial', 'current', 'bin', 'expires'))
+        self.tree.column("#0", width=75)
+        self.tree.column("timestamp", width=100)
+        self.tree.column("initial", width=50)
+        self.tree.column("current", width=50)
+        self.tree.column("bin", width=50)
+        self.tree.column("expires", width=50)
 
-        t.tag_configure('won', foreground='#006400', background='grey')
-        t.tag_configure('bid', foreground='#006400')
-        t.tag_configure('war', foreground='#B77600')
-        t.tag_configure('sold', foreground='#1C7CA9', background='grey')
-        t.tag_configure('lost', foreground='#B70000', background='grey')
+        self.tree.heading("#0", text="Name", anchor="w")
+        self.tree.heading("timestamp", text="Time")
+        self.tree.heading("initial", text="Initial Bid")
+        self.tree.heading("current", text="Current Bid")
+        self.tree.heading("bin", text="BIN")
+        self.tree.heading("expires", text="Expires")
 
-        self.view = t
+        self.tree.tag_configure('won', foreground='#006400', background='grey')
+        self.tree.tag_configure('bid', foreground='#006400')
+        self.tree.tag_configure('war', foreground='#B77600')
+        self.tree.tag_configure('sold', foreground='#1C7CA9', background='grey')
+        self.tree.tag_configure('lost', foreground='#B70000', background='grey')
+
+        # scrollbar
+        ysb = ttk.Scrollbar(self.view, orient='vertical', command=self.tree.yview)
+        self.tree.configure(yscroll=ysb.set)
+        self.tree.grid(row=0, column=0, sticky='news')
+        ysb.grid(row=0, column=1, sticky='ns')
 
     def get_view(self):
         return self.view
@@ -44,7 +54,7 @@ class Auctions():
     def add_auction(self, card, timestamp, currbid, index='end', tag=''):
         if not card.cardid in self.cards:
             self.cards[card.cardid] = card
-            return self.view.insert("", index, card.cardid, text=card.cardname, values=(timestamp, card.startingBid,
+            return self.tree.insert("", index, card.cardid, text=card.cardname, values=(timestamp, card.startingBid,
                                                                                         currbid, card.buyNowPrice,
                                                                                         card.expires), tags=(tag,))
 
@@ -52,15 +62,15 @@ class Auctions():
         if not card.cardid in self.cards:
             self.add_auction(card, timestamp, currbid, 'end', tag)
         else:
-            options = self.view.item(card.cardid)
+            options = self.tree.item(card.cardid)
             options['values'] = (timestamp, card.startingBid,
                                  currbid, card.buyNowPrice,
                                  card.expires)
             if tag:
                 options['tags'] = (tag,)
-            self.view.item(card.cardid, text=options['text'], values=options['values'], tags=options['tags'])
-        self.view.see(card.cardid)
-        self.view.selection_set([card.cardid])
+            self.tree.item(card.cardid, text=options['text'], values=options['values'], tags=options['tags'])
+        self.tree.see(card.cardid)
+        self.tree.selection_set([card.cardid])
 
 class Card():
 
@@ -68,11 +78,9 @@ class Card():
         self.cardid = item['id']
         self.resourceId = item['resourceId']
         self.tradeId = item['tradeId']
-        self.cardType = item['itemType']
         self.buyNowPrice = item['buyNowPrice'] if item['buyNowPrice'] is not None else item['lastSalePrice']
         self.startingBid = item['startingBid'] if item['startingBid'] is not None else "BIN"
         self.currentBid = item['currentBid'] if item['currentBid'] is not None else item['lastSalePrice']
-        self.contract = item['contract']
         self.expires = item['expires'] if item['expires'] is not None else -1
 
 class PlayerCard(Card):
